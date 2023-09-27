4fce67062916e75161237f54604500542604da13
azuolasada <144427200+azuolasada@users.noreply.github.com>
2023-09-27 19:28:49 +0000

    Added NaN homework exception.

--- v0.1.cpp
+++ v0.1.cpp
@@ -46,106 +46,134 @@ int generateRandomScore(int min, int max) {
 }
 
 int main() {
-    std::vector<Student> students;
-    char calculationMethod;
-    char inputMethod;
+    try {
+        std::vector<Student> students;
+        char calculationMethod;
+        char inputMethod;
 
-    std::cout << "Choose calculation method (m for mean, d for median): ";
-    std::cin >> calculationMethod;
-    std::cin.ignore();
-
-    std::cout << "Choose input method (i for manual input, r for random, f for file): ";
-    std::cin >> inputMethod;
-    std::cin.ignore();
-
-    if (inputMethod == 'f') {
-        std::ifstream file("kursiokai.txt");
-        if (!file) {
-            std::cerr << "Error opening file kursiokai.txt!" << std::endl;
-            return 1;
+        std::cout << "Choose calculation method (m for mean, d for median): ";
+        std::cin >> calculationMethod;
+        if (calculationMethod != 'm' && calculationMethod != 'd') {
+            throw std::runtime_error("Invalid calculation method selected!");
         }
-
-        // Ignore header line
-        std::string header;
-        std::getline(file, header);
-
-        while (file) {
-            Student student;
-            file >> student.name >> student.surname;
-
-            // If we reached the end of file after reading name and surname, exit loop
-            if (file.eof()) break;
-
-            // Read homework scores and exam result
-            for (int i = 0; i < 15; ++i) {
-                int score;
-                file >> score;
-                student.homeworkResults.push_back(score);
-            }
-            file >> student.examResult;
-
-            // Ignore rest of line (if there's anything left)
-            std::getline(file, header);
-
-            students.push_back(student);
-        }
-    } else {
-        int numStudents;
-        std::cout << "Enter the number of students: ";
-        std::cin >> numStudents;
         std::cin.ignore();
 
-        for (int i = 0; i < numStudents; ++i) {
-            Student student;
+        std::cout << "Choose input method (i for manual input, r for random, f for file): ";
+        std::cin >> inputMethod;
+        if (inputMethod != 'i' && inputMethod != 'r' && inputMethod != 'f') {
+            throw std::runtime_error("Invalid input method selected!");
+        }
+        std::cin.ignore();
 
-            std::cout << "Enter name for student " << (i + 1) << ": ";
-            std::getline(std::cin, student.name);
-
-            std::cout << "Enter surname for student " << (i + 1) << ": ";
-            std::getline(std::cin, student.surname);
-
-            if (inputMethod == 'i') {
-                std::cout << "Enter homework results for student " << (i + 1) << " (press Enter twice to stop):\n";
-                while (true) {
-                    std::string line;
-                    std::getline(std::cin, line);
-                    if (line.empty()) {
-                        break;
-                    }
-                    std::istringstream iss(line);
-                    int result;
-                    iss >> result;
-                    student.homeworkResults.push_back(result);
-                }
-
-                std::cout << "Enter exam result for student " << (i + 1) << ": ";
-                std::cin >> student.examResult;
-                std::cin.ignore();
-            } else if (inputMethod == 'r') {
-                int numHomeworks = generateRandomScore(3, 15);
-                for (int j = 0; j < numHomeworks; ++j) {
-                    student.homeworkResults.push_back(generateRandomScore(1, 10));
-                }
-
-                student.examResult = generateRandomScore(1, 10);
-                std::cout << "Generated random scores for " << student.name << " " << student.surname << std::endl;
+        if (inputMethod == 'f') {
+            std::ifstream file("kursiokai.txt");
+            if (!file) {
+                throw std::runtime_error("Error opening file kursiokai.txt!");
             }
 
-            students.push_back(student);
+            // Ignore header line
+            std::string header;
+            std::getline(file, header);
+
+            while (file) {
+                Student student;
+                file >> student.name >> student.surname;
+
+                // If we reached the end of file after reading name and surname, exit loop
+                if (file.eof()) break;
+
+                // Read homework scores and exam result
+                for (int i = 0; i < 15; ++i) {
+                    int score;
+                    if (!(file >> score)) {
+                        throw std::runtime_error("Error reading score from file!");
+                    }
+                    student.homeworkResults.push_back(score);
+                }
+                if (!(file >> student.examResult)) {
+                    throw std::runtime_error("Error reading exam result from file!");
+                }
+
+                // Ignore rest of line (if there's anything left)
+                std::getline(file, header);
+
+                if (student.homeworkResults.empty()) {
+                    std::cout << "No homework results for " << student.name << " " << student.surname << " in the file. It's not possible to do calculations." << std::endl;
+                    continue;
+                }
+
+                students.push_back(student);
+            }
+        } else {
+            int numStudents;
+            std::cout << "Enter the number of students: ";
+            std::cin >> numStudents;
+            if (numStudents <= 0) {
+                throw std::runtime_error("Number of students should be positive!");
+            }
+            std::cin.ignore();
+
+            for (int i = 0; i < numStudents; ++i) {
+                Student student;
+                std::cout << "Enter name for student " << (i + 1) << ": ";
+                std::getline(std::cin, student.name);
+
+                std::cout << "Enter surname for student " << (i + 1) << ": ";
+                std::getline(std::cin, student.surname);
+
+                if (inputMethod == 'i') {
+                    std::cout << "Enter homework results for student " << (i + 1) << " (press Enter twice to stop):\n";
+                    while (true) {
+                        std::string line;
+                        std::getline(std::cin, line);
+                        if (line.empty()) {
+                            break;
+                        }
+                        std::istringstream iss(line);
+                        int result;
+                        iss >> result;
+                        student.homeworkResults.push_back(result);
+                    }
+
+                    if (student.homeworkResults.empty()) {
+                        std::cout << "No homework results for " << student.name << " " << student.surname << ". It's not possible to do calculations." << std::endl;
+                        continue;
+                    }
+
+                    std::cout << "Enter exam result for student " << (i + 1) << ": ";
+                    std::cin >> student.examResult;
+                    std::cin.ignore();
+                } else if (inputMethod == 'r') {
+                    int numHomeworks = generateRandomScore(3, 15);
+                    for (int j = 0; j < numHomeworks; ++j) {
+                        student.homeworkResults.push_back(generateRandomScore(1, 10));
+                    }
+
+                    student.examResult = generateRandomScore(1, 10);
+                    std::cout << "Generated random scores for " << student.name << " " << student.surname << std::endl;
+                }
+
+                students.push_back(student);
+            }
         }
-    }
 
-    printDataFrameHeader();
-    for (const auto &student : students) {
-        double homeworkScore = (calculationMethod == 'm' ? 
-            computeMean(student.homeworkResults) : computeMedian(student.homeworkResults));
-        double finalScore = 0.4 * homeworkScore + 0.6 * student.examResult;
+        printDataFrameHeader();
+        for (const auto &student : students) {
+            double homeworkScore = (calculationMethod == 'm' ? 
+                computeMean(student.homeworkResults) : computeMedian(student.homeworkResults));
+            double finalScore = 0.4 * homeworkScore + 0.6 * student.examResult;
 
-        std::cout << std::left << std::setw(15) << student.name
-                  << std::setw(15) << student.surname
-                  << std::setw(15) << finalScore
-                  << std::endl;
+            std::cout << std::left << std::setw(15) << student.name
+                      << std::setw(15) << student.surname
+                      << std::setw(15) << finalScore
+                      << std::endl;
+        }
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
     }
 
     return 0;
 }
+
+
