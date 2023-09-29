3eee1ffda2fb79d3a885014eeb56ea2dc1abe9ab
azuolasada <144427200+azuolasada@users.noreply.github.com>
2023-09-29 06:34:18 +0000

    Added comments

--- random_score.cpp
+++ random_score.cpp
@@ -2,8 +2,12 @@
 #include <random>
 #include <ctime>
 
+// Function to generate a random score between min and max inclusive
 int generateRandomScore(int min, int max) {
-    static std::mt19937 rng(std::time(nullptr));  // Seed the random number generator
-    std::uniform_int_distribution<int> dist(min, max);
+    // Initialize the random number generator with a seed based on the current time
+    static std::mt19937 rng(std::time(nullptr));
+    std::uniform_int_distribution<int> dist(min, max);  // Define the distribution to be between min and max inclusive
+    
+    // Generate and return a random number in the specified range
     return dist(rng);
 }
--- string_utils.cpp
+++ string_utils.cpp
@@ -1,29 +1,44 @@
 #include "string_utils.h"
 
+
+// Function to extract the numeric part from a string
 int extractNumericPart(const std::string& str) {
     size_t i = 0;
+    
+    // Iterate until the first digit is found
     while (i < str.size() && !isdigit(str[i])) {
         ++i;
     }
 
+    // If no digits are found, return -1
     if (i == str.size()) {
         return -1;  // No numeric part found.
     }
 
+    // Identify the start of the numeric segment
     size_t start = i;
+
+    // Continue iterating until the end of the numeric segment
     while (i < str.size() && isdigit(str[i])) {
         ++i;
     }
 
+    // Convert the extracted numeric segment to an integer and return it
     return std::stoi(str.substr(start, i - start));
 }
 
+// Function to extract the non-numeric part from a string
 std::string extractNonNumericPart(const std::string& str) {
     std::string result;
+
+    // Iterate through the string
     for (char c : str) {
+        // If the character is not a digit, add it to the result string
         if (!isdigit(c)) {
             result += c;
         }
     }
+
+    // Return the concatenated non-numeric segment of the string
     return result;
 }
