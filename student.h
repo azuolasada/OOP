cc9891860a97234384e07da8bff3ccebec8cf5b5
azuolasada <144427200+azuolasada@users.noreply.github.com>
2023-11-14 21:33:30 +0000

    Structure to Class

--- student.h
+++ student.h
@@ -1,8 +1,30 @@
-#pragma once
 #include <string>
 #include <vector>
 
-struct Student {
+class Student {
+public:
+    // Constructors
+    Student() = default;
+    Student(const std::string& name, const std::string& surname, int examResult)
+        : name(name), surname(surname), examResult(examResult) {}
+
+    // Public interface for accessing and modifying private data
+    void setName(const std::string& newName) { name = newName; }
+    std::string getName() const { return name; }
+
+    void setSurname(const std::string& newSurname) { surname = newSurname; }
+    std::string getSurname() const { return surname; }
+
+    void setExamResult(int newExamResult) { examResult = newExamResult; }
+    int getExamResult() const { return examResult; }
+
+    void addHomeworkResult(int result) { homeworkResults.push_back(result); }
+    const std::vector<int>& getHomeworkResults() const { return homeworkResults; }
+
+    void setFinalScore(double newScore) { finalScore = newScore; }
+    double getFinalScore() const { return finalScore; }
+
+private:
     std::string name;
     std::string surname;
     std::vector<int> homeworkResults;
