09966888fd303fb12402b781fa0c86f12c9a2d67
azuolasada <144427200+azuolasada@users.noreply.github.com>
2023-12-03 13:53:57 +0000

    Student.h changes

--- student.h
+++ student.h
@@ -1,42 +1,39 @@
-#ifndef STUDENT_H
-#define STUDENT_H
-
-#include <iostream>
 #include <string>
 #include <vector>
+#include <iostream>
 
 class Student {
 public:
     // Constructors
-    Student() = default;
-    Student(const std::string& name, const std::string& surname, int examResult)
-        : name(name), surname(surname), examResult(examResult), finalScore(0.0) {}
-
-    // Rule of Three
-    ~Student() = default;
-    Student(const Student& other) = default;
-    Student& operator=(const Student& other) = default;
+    Student();
+    Student(const std::string& name, const std::string& surname, int examResult);
+    // Copy constructor
+    Student(const Student& other);
+    // Destructor
+    ~Student();
+    // Copy assignment operator
+    Student& operator=(const Student& other);
 
     // Public interface for accessing and modifying private data
-    void setName(const std::string& newName) { name = newName; }
-    std::string getName() const { return name; }
+    void setName(const std::string& newName);
+    std::string getName() const;
 
-    void setSurname(const std::string& newSurname) { surname = newSurname; }
-    std::string getSurname() const { return surname; }
+    void setSurname(const std::string& newSurname);
+    std::string getSurname() const;
 
-    void setExamResult(int newExamResult) { examResult = newExamResult; }
-    int getExamResult() const { return examResult; }
+    void setExamResult(int newExamResult);
+    int getExamResult() const;
 
-    void addHomeworkResult(int result) { homeworkResults.push_back(result); }
-    const std::vector<int>& getHomeworkResults() const { return homeworkResults; }
-    void removeLastHomeworkResult() { if (!homeworkResults.empty()) homeworkResults.pop_back(); }
+    void addHomeworkResult(int result);
+    const std::vector<int>& getHomeworkResults() const;
+    void removeLastHomeworkResult();
 
-    void setFinalScore(double newScore) { finalScore = newScore; }
-    double getFinalScore() const { return finalScore; }
+    void setFinalScore(double newScore);
+    double getFinalScore() const;
 
-    // Input/Output operators
-    friend std::istream& operator>>(std::istream& in, Student& student);
-    friend std::ostream& operator<<(std::ostream& out, const Student& student);
+    // Friend functions for input/output operators
+    friend std::ostream& operator<<(std::ostream& os, const Student& student);
+    friend std::istream& operator>>(std::istream& is, Student& student);
 
 private:
     std::string name;
@@ -45,5 +42,3 @@ private:
     int examResult;
     double finalScore;
 };
-
-#endif // STUDENT_H
