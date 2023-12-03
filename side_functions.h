755078954b1e1ac30eb157fa60815b01b143bdbf
azuolasada <144427200+azuolasada@users.noreply.github.com>
2023-12-03 13:42:40 +0000

    Side_functions.h update

--- side_functions.h
+++ side_functions.h
@@ -3,6 +3,7 @@
 #include "student.h"
 #include <vector>
 #include <list>
+#include <iostream>
 
 template <typename Container>
 void processStudents(Container& students, const std::string& filename, int strategy);
@@ -18,4 +19,6 @@ void writeStudentsToFile(const Container& students, const std::string& filename)
 
 void generateStudentFile(const std::string& filename, size_t numRecords);
 
-
+// Input and Output operators for Student class
+std::istream& operator>>(std::istream& in, Student& student);
+std::ostream& operator<<(std::ostream& out, const Student& student);
