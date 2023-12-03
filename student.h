4d6c5cebcf47858ee7645a54fa139b5caa60b1d3
azuolasada <144427200+azuolasada@users.noreply.github.com>
2023-12-03 14:34:55 +0000

    student.h update

--- student.h
+++ student.h
@@ -5,17 +5,22 @@
 
 class Student {
 public:
-    // Constructors
-    Student();
-    Student(const std::string& name, const std::string& surname, int examResult);
-    // Copy constructor
-    Student(const Student& other);
-    // Destructor
-    ~Student();
-    // Copy assignment operator
-    Student& operator=(const Student& other);
+    // Default constructor
+    Student() = default; // Use the compiler-generated default constructor
 
-    // Public interface for accessing and modifying private data
+    // Parameterized constructor
+    Student(const std::string& name, const std::string& surname, int examResult);
+
+    // Copy constructor
+    Student(const Student& other) = default; // Use the compiler-generated copy constructor
+
+    // Destructor
+    ~Student() = default; // Use the compiler-generated destructor
+
+    // Copy assignment operator
+    Student& operator=(const Student& other) = default; // Use the compiler-generated copy assignment operator
+
+    // Public methods to set and get student's details
     void setName(const std::string& newName);
     std::string getName() const;
 
@@ -25,21 +30,28 @@ public:
     void setExamResult(int newExamResult);
     int getExamResult() const;
 
+    // Method to add a homework result
     void addHomeworkResult(int result);
+
+    // Method to get all homework results
     const std::vector<int>& getHomeworkResults() const;
+
+    // Method to remove the last homework result (assuming it's the exam result)
     void removeLastHomeworkResult();
 
+    // Method to set and get the final score
     void setFinalScore(double newScore);
     double getFinalScore() const;
 
-    // Friend functions for input/output operators
+    // Friend declarations for input/output operators
     friend std::ostream& operator<<(std::ostream& os, const Student& student);
     friend std::istream& operator>>(std::istream& is, Student& student);
 
 private:
-    std::string name;
-    std::string surname;
-    std::vector<int> homeworkResults;
-    int examResult;
-    double finalScore;
+    // Member variables
+    std::string name; // Student's name
+    std::string surname; // Student's surname
+    std::vector<int> homeworkResults; // Container for homework results
+    int examResult; // Exam result
+    double finalScore; // Final calculated score
 };
