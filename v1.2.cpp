6d69e8ec2c36ac27445c10645aaa60a3f2d8e5b1
azuolasada <144427200+azuolasada@users.noreply.github.com>
2023-12-03 13:50:21 +0000

    v1.2.cpp update

--- v1.2.cpp
+++ v1.2.cpp
@@ -2,6 +2,7 @@
 #include <list>
 #include <vector>
 #include "side_functions.h"
+#include "student.h"
 
 int main() {
     try {
@@ -9,12 +10,17 @@ int main() {
         std::cout << "Select container type (l for list, v for vector): ";
         std::cin >> containerType;
 
+        if (containerType != 'l' && containerType != 'v') {
+            std::cerr << "Invalid container type selected! Must be 'l' or 'v'." << std::endl;
+            return 1;
+        }
+
         int strategy;
         std::cout << "Select strategy (1 for Strategy 1, 2 for Strategy 2, 3 for Strategy 3): ";
         std::cin >> strategy;
 
         if (strategy != 1 && strategy != 2 && strategy != 3) {
-            std::cerr << "Invalid strategy number!" << std::endl;
+            std::cerr << "Invalid strategy number! Must be 1, 2, or 3." << std::endl;
             return 1;
         }
 
@@ -26,17 +32,16 @@ int main() {
             if (containerType == 'l') {
                 std::list<Student> students;
                 processStudents(students, filename, strategy);
-            } else if (containerType == 'v') {
+            } else { // Only other option is 'v', already checked earlier
                 std::vector<Student> students;
                 processStudents(students, filename, strategy);
-            } else {
-                std::cerr << "Invalid container type selected!" << std::endl;
-                return 1;
             }
         }
     } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
+
+    std::cout << "Processing completed successfully." << std::endl;
     return 0;
 }
