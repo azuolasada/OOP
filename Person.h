8036787d9245abbe1df6dde130aad0894fa9efbc
azuolasada <144427200+azuolasada@users.noreply.github.com>
2023-12-22 07:48:28 +0000

    Person.h comments

--- Person.h
+++ Person.h
@@ -1,25 +1,55 @@
 #pragma once
 #include <string>
 
+/**
+ * @brief Represents a person with a name and surname.
+ */
 class Person {
 protected:
-    std::string name;
-    std::string surname;
+    std::string name; ///< Name of the person
+    std::string surname; ///< Surname of the person
 
 public:
-    // Constructor
+    /**
+     * @brief Construct a new Person object
+     * 
+     * @param name The name of the person
+     * @param surname The surname of the person
+     */
     Person(const std::string& name, const std::string& surname)
         : name(name), surname(surname) {}
 
-    // Virtual destructor
+    /**
+     * @brief Virtual destructor for the Person class
+     */
     virtual ~Person() {}
 
-    // Getters
+    /**
+     * @brief Get the Name of the person
+     * 
+     * @return const std::string& Name of the person
+     */
     const std::string& getName() const { return name; }
+
+    /**
+     * @brief Get the Surname of the person
+     * 
+     * @return const std::string& Surname of the person
+     */
     const std::string& getSurname() const { return surname; }
 
-    // Setters
+    /**
+     * @brief Set the Name of the person
+     * 
+     * @param newName New name for the person
+     */
     void setName(const std::string& newName) { name = newName; }
+
+    /**
+     * @brief Set the Surname of the person
+     * 
+     * @param newSurname New surname for the person
+     */
     void setSurname(const std::string& newSurname) { surname = newSurname; }
 
 };
